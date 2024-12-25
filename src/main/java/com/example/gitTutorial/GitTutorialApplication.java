09e9de17b7a88cc2c5493eb970d610af75226bbc
sasidharyadavalli <sasidharsasi218@gmail.com>
2024-12-25 05:51:49 +0530

    added springboot initial files

---
+++ src/main/java/com/example/gitTutorial/GitTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitTutorial/GitTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
